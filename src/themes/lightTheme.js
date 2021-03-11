d376b71122fb0b479440b4408e5d2e5af395e16a
ebenKB <ekb.adjei@yahoo.com>
2021-03-11 16:16:02 +0000

    initial commit

---
+++ src/themes/lightTheme.js
@@ -0,0 +1,18 @@
+export const themes = {
+  basic: {
+    palette: {
+      primary: {
+        main: '#000',
+        secondary: '#abe'
+      }
+    },
+    text: {
+      main: '#000',
+      secondary: '#ccb',
+      faint: '#bbb',
+    },
+    icons: {
+
+    }
+  }
+}
